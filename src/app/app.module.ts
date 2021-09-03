d5fe2998978a6948cf878525775631bc62fd6677
sanobar28 <sanobar.mujawar@gmail.com>
2021-09-03 08:43:54 +0530

    added: created project with required assets and folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddComponent } from './component/add/add.component';
+import { HomeComponent } from './component/home/home.component';
+import { HeaderComponent } from './component/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
